6db25b9f2a777a62c182d116da32d684bee42194
Karivool <themataaa@gmail.com>
2016-12-23 01:16:40 -0500

    Modify index.jsx

--- dev/index.jsx
+++ dev/index.jsx
@@ -4,19 +4,16 @@ import ReactDOM from "react-dom";
 let bingoCard = React.createClass({
   render: function() {
     return (
-      <p>Hello, {this.props.greetTarget}!</p>
+      <div>
+        <p>{this}</p>
+      </div>
     );
   }
 });
 
 ReactDOM.render(
   <div>
-    <bingoCard greetTarget="Pizza Lady"/>
-    <bingoCard greetTarget="Bungh0"/>
-    <bingoCard greetTarget="Br00klyn Lady"/>
-    <bingoCard greetTarget="Gr0ce"/>
-    <bingoCard greetTarget="Swede"/>
-    <bingoCard greetTarget="Jesterwalky"/>
+    Pizza Lady!
   </div>,
   document.querySelector("#container")
 );
