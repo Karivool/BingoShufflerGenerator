397ad2ef56e3069be60758d1efe9dfa02651fc80
Karivool <themataaa@gmail.com>
2016-12-25 22:15:23 -0500

    Update default bingo spaces

---
+++ constants/rrelationships.js
@@ -0,0 +1,29 @@
+const RRelationships = {
+  spaces: ["Open up the relationship",
+  "Btw, someone's pregnant",
+  "Hardcore gamer",
+  "Doesn't wash body properly",
+  "Age discrepancy",
+  "Property destruction",
+  "Bodily fluids",
+  "Picky eater",
+  "First relationship",
+  "It's just a prank/joke bro",
+  "Devastated about porn",
+  "Cheated, didn't sever",
+  "Wedding bullshit",
+  "Greedy in-laws/family",
+  "On the spectrum",
+  "Physical/sexual abuse",
+  "Denied sex act others got",
+  "Happy fuckin holidays",
+  "Sentence ends with '[person] and I'",
+  "The OP is the problem",
+  "The OP is oblivious",
+  "Weird fetish",
+  "A non-issue is mentioned",
+  "Double standards",
+  "Everyone is terrible"]
+};
+
+module.exports = RRelationships;
